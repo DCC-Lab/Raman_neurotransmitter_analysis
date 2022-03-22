95b7d10028e1de5682ceea9e8d36e152380238af
AntoineRousseau97 <antoine.rousseau.2@ulaval.ca>
2022-03-22 11:31:15 -0400

    New code structure
    The code is now seperated in 4 classes:
    OceanViewSpectralFile -> take care of a single file
    Acquisition -> calls OceanViewSpectralFiles in order to get the spectra and their info in a object called 'Spectrum' or 'Spectra'
    Spectrum -> used to do stuff on a spectrum
    Spectra -> used to do stuff on spectra

--- spectral_analysis/do_stuff.py
+++ spectral_analysis/do_stuff.py
@@ -1,142 +1,36 @@
 import spectrum
-import os
+import matplotlib.pyplot as plt
+
+# A NEW ERA HAS BEGUN!!!!!!!!!!!!!!!!!!!!!!!
+
+bg = spectrum.Acquisition('/Users/antoinerousseau/Desktop/20220223/backgrounds/10min_0light/').spectraSum()
+
+water = spectrum.Acquisition('/Users/antoinerousseau/Desktop/20220223/eau_30sec/').spectraSum()
+water.removeThermalNoise(bg)
+# water.normalizeCounts()
+# water.display()
+
+dopamine = spectrum.Acquisition('/Users/antoinerousseau/Desktop/20220315/dopamine/').spectra()
+dopamine.removeThermalNoise(bg)
+dopamine.subtract(water)
+dopamine.normalizeCounts()
+dopamine.display()
+
+GABA = spectrum.Acquisition('/Users/antoinerousseau/Desktop/20220315/GABA/')
+GABAsum = GABA.spectra()
+# GABAsum.removeThermalNoise(bg)
+# GABAsum.display()
+# GABAsum.normalizeCounts()
+GABAsum.subtract(water)
+# GABAsum.display()
 
 
-my_data = {}
-path = '/Users/antoinerousseau/Desktop/PM/'
-for i in os.listdir(path):
-    if i[0] == '.':
-        continue
-    if i == 'backgrounds':
-        continue
-    name = '{}'.format(i)
-    my_data[name] = my_data.get(name, spectrum.Spectrum(path + i + '/'))
-
-path = '/Users/antoinerousseau/Desktop/ERIKA/'
-for i in os.listdir(path):
-    if i[0] == '.':
-        continue
-    if i == 'backgrounds':
-        continue
-    name = '{}'.format(i)
-    my_data[name] = my_data.get(name, spectrum.Spectrum(path + i + '/'))
-
-path = '/Users/antoinerousseau/Desktop/20220124/'
-for i in os.listdir(path):
-    if i[0] == '.':
-        continue
-    if i == 'backgrounds':
-        continue
-    name = '{}'.format(i)
-    my_data[name] = my_data.get(name, spectrum.Spectrum(path + i + '/'))
-
-path = '/Users/antoinerousseau/Desktop/ERIKA/backgrounds/'
-for i in os.listdir(path):
-    if i[0] == '.':
-        continue
-    if i == 'backgrounds':
-        continue
-    name = '{}'.format(i)
-    my_data[name] = my_data.get(name, spectrum.Spectrum(path + i + '/'))
+# plt.plot(dopamine.wavenumber, dopamine.counts, label='GABA')
+# plt.plot(dopamine.wavenumber, y, label='GABA - water')
+# plt.legend()
+# plt.show()
 
 
 
-path = '/Users/antoinerousseau/Desktop/20220223/'
-for i in os.listdir(path):
-    if i[0] == '.':
-        continue
-    if i == 'backgrounds':
-        continue
-    name = '{}'.format(i)
-    my_data[name] = my_data.get(name, spectrum.Spectrum(path + i + '/'))
 
-
-
-bg_abs = spectrum.Spectrum('/Users/antoinerousseau/Desktop/20220223/backgrounds/10min_0light/')
-bg_nolight = spectrum.Spectrum('/Users/antoinerousseau/Desktop/20220223/backgrounds/10min_lightoff/')
-bg_light = spectrum.Spectrum('/Users/antoinerousseau/Desktop/20220223/backgrounds/10min_lighton/')
-
-# spectrum.Spectrum.customPlot(bg_abs.x[0], bg_nolight.y, bg_abs.y)
-
-my_data['eau_30sec'].substract(bg_nolight.y[0], time=30)
-sum_water = my_data['eau_30sec'].sum_y[0]
-
-
-#big database
-
-# my_data['cm_30sec_lightoff_1A_quartz_45uL'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lightoff_1A_2'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lighton_1A_quartz_45uL'].substract(bg_light.y[0], time=30)
-# my_data['cm_30sec_lightoff_1B_quartz_45uL'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lightoff_1B_2'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lighton_1B_quartz_45uL'].substract(bg_light.y[0], time=30)
-#
-# my_data['cm_30sec_lightoff_2A_quartz_45uL'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lightoff_2A_2'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lighton_2A_quartz_45uL'].substract(bg_light.y[0], time=30)
-# my_data['cm_30sec_lightoff_2B_quartz_45uL'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lightoff_2B_2'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lighton_2B_quartz_45uL'].substract(bg_light.y[0], time=30)
-#
-# my_data['cm_30sec_lightoff_3A_quartz_45uL'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lightoff_3A_2'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lighton_3A_quartz_45uL'].substract(bg_light.y[0], time=30)
-# my_data['cm_30sec_lightoff_3B_quartz_45uL'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lightoff_3B_2'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lighton_3B_quartz_45uL'].substract(bg_light.y[0], time=30)
-#
-# my_data['cm_30sec_lightoff_4A_quartz_45uL'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lightoff_4A_2'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lighton_4A_quartz_45uL'].substract(bg_light.y[0], time=30)
-# my_data['cm_30sec_lightoff_4B_quartz_45uL'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lightoff_4B_2'].substract(bg_nolight.y[0], time=30)
-# my_data['cm_30sec_lighton_4B_quartz_45uL'].substract(bg_light.y[0], time=30)
-#
-# my_data['cm_30sec_lightoff_1A_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lightoff_1A_2'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lighton_1A_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lightoff_1B_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lightoff_1B_2'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lighton_1B_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-#
-# my_data['cm_30sec_lightoff_2A_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lightoff_2A_2'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lighton_2A_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lightoff_2B_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lightoff_2B_2'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lighton_2B_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-#
-# my_data['cm_30sec_lightoff_3A_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lightoff_3A_2'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lighton_3A_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lightoff_3B_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lightoff_3B_2'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lighton_3B_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-#
-# my_data['cm_30sec_lightoff_4A_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lightoff_4A_2'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lighton_4A_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lightoff_4B_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lightoff_4B_2'].substract(sum_water, bg_acq_time=300, time=30)
-# my_data['cm_30sec_lighton_4B_quartz_45uL'].substract(sum_water, bg_acq_time=300, time=30)
-#
-# #spectrum.Spectrum.customPlot(my_data['cm_30sec_lightoff_1A_quartz_45uL'].x[0], my_data['cm_30sec_lightoff_1A_quartz_45uL'].sum_y, my_data['cm_30sec_lightoff_1B_quartz_45uL'].sum_y, my_data['cm_30sec_lightoff_2A_quartz_45uL'].sum_y, my_data['cm_30sec_lightoff_2B_quartz_45uL'].sum_y, my_data['cm_30sec_lightoff_3A_quartz_45uL'].sum_y, my_data['cm_30sec_lightoff_3B_quartz_45uL'].sum_y, my_data['cm_30sec_lightoff_4A_quartz_45uL'].sum_y, my_data['cm_30sec_lightoff_4B_quartz_45uL'].sum_y)
-# spectrum.Spectrum.customPlot(my_data['cm_30sec_lighton_1A_quartz_45uL'].x[0], my_data['cm_30sec_lighton_1A_quartz_45uL'].sum_y, my_data['cm_30sec_lighton_1B_quartz_45uL'].sum_y, my_data['cm_30sec_lighton_2A_quartz_45uL'].sum_y, my_data['cm_30sec_lighton_2B_quartz_45uL'].sum_y, my_data['cm_30sec_lighton_3A_quartz_45uL'].sum_y, my_data['cm_30sec_lighton_3B_quartz_45uL'].sum_y, my_data['cm_30sec_lighton_4A_quartz_45uL'].sum_y, my_data['cm_30sec_lighton_4B_quartz_45uL'].sum_y)
-
-
-# ETHANOOOOOLLLL
-# my_data['ethanol_10sec'].substract(bg_abs.y[0], time=10)
-# my_data['ethanol_10sec'].plotSpec(WN=True)
-#
-# my_data['vin_1'].substract(bg_abs.y[0], time=2.5)
-# my_data['vin_1'].plotSpec(sum=True, WN=True)
-
-
-my_data['quartz_coverslip_10sec_peak'].substract(bg_abs.y[0], time=10)
-my_data['quartz_coverslip_10sec_peak'].factor(6)
-my_data['eau_distillee_30sec_1'].substract(bg_abs.y[0], time=30)
-my_data['eau_distillee_30sec_1'].factor(0.64)
-my_data['cm_30sec_lightoff_2A_quartz_45uL'].substract(bg_abs.y[0], time=30)
-my_data['cm_30sec_lightoff_2A_2'].substract(bg_abs.y[0], time=30)
-spectrum.Spectrum.customPlot(my_data['cm_30sec_lightoff_2A_2'].x[0], my_data['cm_30sec_lightoff_2A_2'].sum_y, my_data['eau_distillee_30sec_1'].sum_y, [my_data['quartz_coverslip_10sec_peak'].y[0]])
 
--- spectral_analysis/spectrum.py
+++ spectral_analysis/spectrum.py
@@ -6,144 +6,297 @@ from sklearn.metrics import r2_score
 from scipy.optimize import curve_fit
 
 
-class Spectrum:
+class OceanViewSpectralFile:
 
-    def __init__(self, directory: str):
-        self.directory = directory
+    def __init__(self, filepath: str):
+        self.filepath = filepath
         self.x = []
         self.y = []
-        self.sum_y = [np.zeros(1044)]
-        self.fileName = []
+        self.date = None
+        self.trig_mode = None
+        self.integration_time = None
+        self.dark_corr = None
+        self.nonlin_corr = None
+        self.x_axis_unit = None
+        self.pixel_nb = None
+        self.fileName = filepath.split('/')[-1]
+        self.spectrum = None
+        self.README = None
 
-        self._load()
-        self.sumData()
+        self._loadSpectrumValues()
+        self.isValid()
+        self._loadAcquisitionDetails()
+
+
+
+    def isValid(self, pixelNb = 1044):
+        # Is it a .txt file
+        fileType = self.filepath.split('/')[-1][-4:]
+        assert fileType == '.txt', 'file type should be .txt'
+
+        # Does it contain expected strings at the right place
+        fich = open(self.filepath, "r")
+        fich_list = list(fich)
+        date = fich_list[2]
+        trig_mode = fich_list[5]
+        integration_time = fich_list[6]
+        dark_corr = fich_list[8]
+        nonlin_corr = fich_list[9]
+        x_axis_unit = fich_list[11]
+        pixel_nb = fich_list[12]
+        fich.close()
+
+        assert date.split(': ')[0] == 'Date', "Was expecting the .txt file\'s 3rd lane to be the date of the acquisition"
+        assert trig_mode.split(': ')[0] == 'Trigger mode', "Was expecting the .txt file\'s 6th lane to be the trigger mode used for the acquisition"
+        assert integration_time.split(': ')[0] == 'Integration Time (sec)', "Was expecting the .txt file\'s 7th lane to be the integration time used (in sec) for the acquisition"
+        assert dark_corr.split(': ')[0] == 'Electric dark correction enabled', "Was expecting the .txt file\'s 9th lane to be the electric dark correction state for the acquisition"
+        assert nonlin_corr.split(': ')[0] == 'Nonlinearity correction enabled', "Was expecting the .txt file\'s 10th lane to be the nonlinearity correction state for the acquisition"
+        assert x_axis_unit.split(': ')[0] == 'XAxis mode', "Was expecting the .txt file\'s 12th lane to be the x axis units for the acquisition"
+        assert pixel_nb.split(': ')[0] == 'Number of Pixels in Spectrum', "Was expecting the .txt file\'s 13th lane to be the number of pixels used for the acquisition"
+
+        # Does it have the right len(x), len(y)
+        assert len(self.x) == pixelNb, "Was expecting {0} x values, {1} were given".format(str(pixelNb), str(len(self.x)))
+        assert len(self.y) == pixelNb, "Was expecting {0} y values, {1} were given".format(str(pixelNb), str(len(self.y)))
+        assert len(self.x) == len(self.y), "x and y values for a spectrum should have the same amount of elements"
+
+        # print('File is valid')
+
+
+    def _loadSpectrumValues(self):
+        fich = open(self.filepath, "r")
+        test_str = list(fich)[14:]
+        fich.close()
+
+        # Nettoyer les informations
+        spectral_data = []
+        for j in test_str:
+            elem_str = j.replace(",", ".").replace("\n", "").replace("\t", ",")
+            elem = elem_str.split(",")
+            self.x.append(float(elem[0]))
+            self.y.append(float(elem[1]))
+            spectral_data.append([float(elem[0]), float(elem[1])])
+        self.spectrum = np.transpose(spectral_data)
+
+
+    def _loadAcquisitionDetails(self):
+        fich = open(self.filepath, "r")
+        fich_list = list(fich)
+        date = fich_list[2]
+        trig_mode = fich_list[5]
+        integration_time = fich_list[6]
+        dark_corr = fich_list[8]
+        nonlin_corr = fich_list[9]
+        x_axis_unit = fich_list[11]
+        pixel_nb = fich_list[12]
+        fich.close()
+
+        year = (date.split(' ')[-1]).replace('\n', '')
+        day = date.split(' ')[-4]
+        if len(day) == 1:
+            day = '0' + day
+        month = date.split(' ')[-5]
+        if month == 'Jan':
+            month = '01'
+        if month == 'Feb':
+            month = '02'
+        if month == 'Mar':
+            month = '03'
+        if month == 'Apr':
+            month = '04'
+        if month == 'May':
+            month = '05'
+        if month == 'Jun':
+            month = '06'
+        if month == 'Jul':
+            month = '07'
+        if month == 'Aug':
+            month = '08'
+        if month == 'Sep':
+            month = '09'
+        if month == 'Oct':
+            month = '10'
+        if month == 'Nov':
+            month = '11'
+        if month == 'Dec':
+            month = '12'
+
+        integration_time = integration_time.split(': ')[-1]
+        integration_time = integration_time.replace('\t', '')
+        multiplier = int(integration_time.split('E')[-1])
+        integration_time = (float(integration_time.split('E')[0])) * (10 ** multiplier)
+
+        dark_corr = dark_corr.split(' ')[-1]
+        if dark_corr == 'true':
+            dark_corr = True
+        if dark_corr == 'false':
+            dark_corr = False
+
+        nonlin_corr = nonlin_corr.split(' ')[-1]
+        if nonlin_corr == 'true':
+            nonlin_corr = True
+        if nonlin_corr == 'false':
+            nonlin_corr = False
+
+        self.date = year + month + day
+        self.trig_mode = int(trig_mode.split(' ')[-1])
+        self.integration_time = integration_time
+        self.dark_corr = dark_corr.replace('\n', '')
+        self.nonlin_corr = nonlin_corr.replace('\n', '')
+        self.x_axis_unit = x_axis_unit.split(': ')[-1].replace('\n', '')
+        self.pixel_nb = int(pixel_nb.split(': ')[-1])
+
+
+
+class Acquisition:
+    def __init__(self, directory):
+        self.directory = directory
+        self.spectralFiles = []
+        self.directoryName = directory.split('/')[-2]
+
+        filePaths = self._listNameOfFiles()
+        for filepath in filePaths:
+            spectralFile = OceanViewSpectralFile(directory + filepath)
+            self.spectralFiles.append(spectralFile)
 
 
     def _listNameOfFiles(self, extension="txt") -> list:
         foundFiles = []
 
         for file in os.listdir(self.directory):
+            if file[0] == '.':
+                continue
+            if file == 'README.txt':
+                #should do stuff like reading it and taking info from it
+                RM = open(self.directory + file, "r")
+                self.README = list(RM)
+                continue
             if fnmatch.fnmatch(file, f'*.{extension}'):
                 foundFiles.append(file)
         return foundFiles
 
 
-    def _load(self):
-        donnees_tot_x = []
-        donnees_tot_y = []
+    def spectra(self):
+        spectra = []
+        for file in self.spectralFiles:
+            spectrum = Spectrum(file.x, file.y, file.integration_time, self.directoryName)
+            spectra.append(spectrum)
 
-        for nom in self._listNameOfFiles():
-            # Nom du fichier à importer
-            fich = open(self.directory + '/' + nom, "r")
-            test_str = list(fich)[14:]
-            fich.close()
-            x_raw = []
-            y_raw = []
-            self.fileName.append(nom)
-            # Nettoyer les informations
-            for j in test_str:
-                elem_str = j.replace(",", ".").replace("\n", "").replace("\t", ",")
-                elem = elem_str.split(",")
-                x_raw.append(float(elem[0]))
-                y_raw.append(float(elem[1]))
-
-            donnees_tot_x.append(x_raw)
-            donnees_tot_y.append(y_raw)
-
-        self.x, self.y = donnees_tot_x, donnees_tot_y
+        return Spectra(spectra)
 
 
-    def directoryName(self):
-        return self.directory.split('/')[-2]
+    def spectraSum(self):
+        count_sum = np.zeros(len(self.spectralFiles[0].x))
+        integrationTime = 0
+        for i in self.spectralFiles:
+            count_sum += np.array(i.y)
+            integrationTime += i.integration_time
 
-    @staticmethod
-    def getSNR(val, bgStart=550, bgEnd=800):
+        return Spectrum(self.spectralFiles[0].x, count_sum, integrationTime, self.directoryName)
+
+
+
+class Spectrum:
+    def __init__(self, wavelenghts, counts, integrationTime, label):
+        self.wavelenghts = wavelenghts
+        self.wavenumbers = ((10 ** 7) * ((1 / 785) - (1 / np.array(self.wavelenghts))))
+        self.counts = counts
+        self.integrationTime = integrationTime
+        self.label = label
+
+
+    def getSNR(self, bgStart=550, bgEnd=800):
         bg_AVG = 0
         for i in range(bgStart, bgEnd):
-            bg_AVG += val[i] / (bgEnd - bgStart)
-        return (np.amax(val) - bg_AVG) / np.sqrt(bg_AVG), np.amax(val), (np.amax(val) - bg_AVG)
+            bg_AVG += self.counts[i] / (bgEnd - bgStart)
+        return (np.amax(self.counts) - bg_AVG) / np.sqrt(bg_AVG), np.amax(self.counts), (np.amax(self.counts) - bg_AVG)
 
 
-    @staticmethod
-    def getWN(x, lambda_0=785):
-        return ((10 ** 7) * ((1 / lambda_0) - (1 / np.array(x))))
-
-
-    def sumData(self):
-        y = np.zeros(1044)
-        for i in self.y:
-            y += np.array(i)
-        self.sum_y = [y]
-
-    def plotSpec(self, sum=False, WN=False, xlabel='Wavelenght [nm]', ylabel='Counts [-]'):
-        if sum == True and WN == True:
-            xlabel = 'Wavenumber [cm-1]'
-            plt.plot(self.getWN(self.x[0]), self.sum_y[0], label=str(len(self.fileName))+' summed files'+', SNR= '+str(self.getSNR(self.sum_y[0])[0])[:5]+', peak = '+str(self.getSNR(self.sum_y[0])[2])[:5])
-        if sum == True and WN == False:
-            plt.plot(self.x[0], self.sum_y[0], label=str(len(self.fileName))+' summed files'+', SNR= '+str(self.getSNR(self.sum_y[0])[0])[:5]+', peak = '+str(self.getSNR(self.sum_y[0])[2])[:5])
-        if sum == False and WN == True:
-            xlabel = 'Wavenumber [cm-1]'
-            for i in range(len(self.fileName)):
-                plt.plot(self.getWN(self.x[i]), self.y[i], label=self.fileName[i]+', SNR= '+str(self.getSNR(self.y[i])[0])[:5]+', peak = '+str(self.getSNR(self.y[i])[2])[:5])
-        if sum == False and WN == False:
-            for i in range(len(self.fileName)):
-                plt.plot(self.x[i], self.y[i], label=self.fileName[i]+', SNR= '+str(self.getSNR(self.y[i])[0])[:5]+', peak = '+str(self.getSNR(self.y[i])[2])[:5])
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.title(self.directoryName())
-        plt.legend()
-        plt.show()
-
-
-    @staticmethod
-    def customPlot(x, *args, WN = False, lambda_0=785, xlabel='Wavelenght [nm]', ylabel='Counts [-]', title=None):
-        first = 0
+    def display(self, WN=False, xlabel='Wavelenght [nm]', ylabel='Counts [-]'):
         if WN == True:
-            x = (10 ** 7) * ((1 / lambda_0) - (1 / np.array(x)))
             xlabel = 'Wavenumber [cm-1]'
-            for i in args:
-                second = 0
-                first += 1
-                for j in i:
-                    second += 1
-                    plt.plot(x, j, label=str(first)+'.'+str(second))
+            plt.plot(self.wavenumbers, self.counts, label=self.label + ', SNR= '+str(self.getSNR()[0])+', peak = '+str(self.getSNR()[1]))
         if WN == False:
-            for i in args:
-                second = 0
-                first += 1
-                for j in i:
-                    second += 1
-                    plt.plot(x, j, label=str(first)+'.'+str(second))
+            plt.plot(self.wavelenghts, self.counts, label=self.label + ', SNR= '+str(self.getSNR()[0])+', peak = '+str(self.getSNR()[1]))
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
-        plt.title(title)
         plt.legend()
         plt.show()
 
 
-    def substract(self, background, bg_acq_time=600, **kwargs): # kwargs expected: time and factor
-        time = 60
-        for key, value in kwargs.items():
-            if key == 'time':
-                time = value
-            else:
-                pass
+    def subtract(self, specToSub, specToSub_AcqTime=1): # kwargs expected: time and factor
+        # le specToSubAcqTime est à changer pour mettre le bon temps d'acquisition du spectre à soustraire si le spectre a soustraire n'est as de type Spectrum
+        acqTime = self.integrationTime
 
-        for i in range(len(self.y)):
-            self.y[i] = (np.array(self.y[i]) - (time * np.array(background) / bg_acq_time))
-            self.y[i] = list(self.y[i])
+        if type(specToSub) != Spectrum:
+            self.counts = list(np.array(self.counts) - (acqTime * np.array(specToSub) / specToSub_AcqTime))
 
-        self.sumData()
+        if type(specToSub) == Spectrum:
+            self.counts = list(np.array(self.counts) - (acqTime * np.array(specToSub.counts)) / specToSub.integrationTime)
+
+
+    def removeThermalNoise(self, TNToRemove):
+        assert type(TNToRemove) == Spectrum, 'Passed argument should be of type Spectrum'
+        acqTime = self.integrationTime
+        specToSub_AcqTime = TNToRemove.integrationTime
+
+        self.counts = list(np.array(self.counts) - (acqTime * np.array(TNToRemove.counts) / specToSub_AcqTime))
 
 
     def factor(self, factor):
-        for i in range(len(self.y)):
-            self.y[i] = (np.array(self.y[i]) * factor)
-            self.y[i] = list(self.y[i])
-
-        self.sumData()
+        self.counts = list(np.array(self.counts) * factor)
 
 
-# ------------------------------------------------------------------------------------------------------------------------
+    def normalizeIntegration(self):
+        integration = 0
+        for i in self.counts:
+            integration += i
+
+        self.counts = list(np.array(self.counts) / integration)
+
+
+    def normalizeCounts(self):
+        self.counts = list(np.array(self.counts) / np.amax(self.counts))
+
+
+class Spectra:
+    def __init__(self, spectra):
+        self.spectra = spectra
+
+
+    def display(self, WN=True):
+        if WN == False:
+            for spectrum in self.spectra:
+                plt.plot(spectrum.wavelenghts, spectrum.counts, label=spectrum.label + ', integration = ' + str(spectrum.integrationTime) + ' s')
+        if WN == True:
+            for spectrum in self.spectra:
+                plt.plot(spectrum.wavenumbers, spectrum.counts, label=spectrum.label + ', integration = ' + str(spectrum.integrationTime) + ' s')
+
+        plt.legend()
+        plt.show()
+
+
+    def removeThermalNoise(self, TNToRemove):
+        for spectrum in self.spectra:
+            spectrum.removeThermalNoise(TNToRemove)
+
+
+    def subtract(self, specToSub, specToSub_AcqTime=None):
+        if type(specToSub) != Spectrum:
+            for spectrum in self.spectra:
+                spectrum.counts = list(np.array(spectrum.counts) - (spectrum.integrationTime * np.array(specToSub) / specToSub_AcqTime))
+
+        if type(specToSub) == Spectrum:
+            for spectrum in self.spectra:
+                spectrum.counts = list(np.array(spectrum.counts) - (spectrum.integrationTime * np.array(specToSub.counts) / specToSub.integrationTime))
+
+
+    def normalizeIntegration(self):
+        for spectrum in self.spectra:
+            spectrum.normalizeIntegration()
+
+
+    def normalizeCounts(self):
+        for spectrum in self.spectra:
+            spectrum.normalizeCounts()
 
 
